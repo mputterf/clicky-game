574f5f4df21a14b9f87e792891350402ab671849
Michael Putterflam <michael.putterflam@gmail.com>
2019-06-20 22:43:11 -0700

    Updates

---
+++ precache-manifest.934f5af2ca9bb567b6d7e8e0af6b2d74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8fe073897ad2e59a5df5ebaa6a958c1",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "de9525ed0a59307514c3",
+    "url": "/clicky-game/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "de9525ed0a59307514c3",
+    "url": "/clicky-game/static/js/main.5577a25f.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
